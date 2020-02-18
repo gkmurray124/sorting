1a88a258c3700eb143e273bab2d4ed9fc2e470ed
John Bradford <JohnBradford@Johns-MacBook-Pro-3.local>
2020-02-18 17:05:27 -0500

    changes

--- mergesort.js
+++ mergesort.js
@@ -15,11 +15,10 @@ function merge(array) {
     let firstHalf = array[0]
     let secondHalf = array[1]
     for(let i = 0; i < firstHalf.length; i++) { 
-        // for(let j = 0; j < secondHalf.length; j++) { 
             if(!secondHalf[i]) { 
                 mergedArray.push(firstHalf[i])
             }
-            else if(firstHalf[i] > secondHalf[i]) { 
+            else if (firstHalf[i] > secondHalf[i]) { 
                 mergedArray.push(secondHalf[i]) 
                 mergedArray.push(firstHalf[i])
             } else { 
@@ -27,6 +26,5 @@ function merge(array) {
                 mergedArray.push(secondHalf[i])
             }
         }
-    // }
     return mergedArray;
 }
